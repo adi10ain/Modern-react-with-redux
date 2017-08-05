01c3fb1ff432a62323a5e5c41a83dbcfcb921c1f
Asas Innab <asas.innab@gmail.com>
2017-08-05 22:05:21 +0200

    Added form validation and displaying it!

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -14,6 +14,7 @@ class PostsNew extends Component {
           type="text"
           {...field.input}
         />
+      {field.meta.error}
       </div>
     );
   }
@@ -42,6 +43,31 @@ class PostsNew extends Component {
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  const TITLE_MIN = 8;
+
+  // Validate the inputs from the 'values'
+  if (!values.title) {
+    errors.title = "Enter a title";
+  }
+  if (values.title && values.title.length < TITLE_MIN) {
+    errors.title = `${TITLE_MIN-values.title.length} characters left!`;
+  }
+  if (!values.categories) {
+    errors.categories = 'Enter some categories';
+  }
+  if (!values.content) {
+    errors.content = 'Enter some content please';
+  }
+
+  // If errors is empty, the form is fine to submit
+  // If errors has *any* properties, redux form assumes form is invalid
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })(PostsNew);
