525ac418317aee37df18ab9ee95e765f57c8a1b7
Asas Innab <asas.innab@gmail.com>
2017-08-05 10:27:36 +0200

    Fetching weather data api calls works!

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
+
 const API_KEY = '691d7b28389548cc6ebb8eb8dd1daf74';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FEATCH_WEATHER';
 
