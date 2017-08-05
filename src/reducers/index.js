79be0496255b475383744836d3a22702b87268ac
Asas Innab <asas.innab@gmail.com>
2017-08-05 21:42:38 +0200

    Implementation of redux-form in posts_new

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,5 @@
-import { combineReducers } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import PostsReducer from './reducer_posts';
-import { createStore, combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
